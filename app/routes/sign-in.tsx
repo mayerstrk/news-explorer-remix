0894818c6660b966eff755dacefe2f33900bad78
Mayer Starkman <mayerstrk@gmail.com>
2024-04-01 15:33:49 +0300

    refactor(users, auth, request-buiolder) split users into auth service and request builder file

--- app/routes/sign-in.tsx
+++ app/routes/sign-in.tsx
@@ -1,4 +1,5 @@
 import { ActionFunctionArgs, redirect } from '@vercel/remix'
+import { signIn } from '~/services/users'
 import { getSession, commitSession } from '~/session.server'
 
 export const loader = () => {
@@ -6,6 +7,13 @@ export const loader = () => {
 }
 
 export const action = async ({ request }: ActionFunctionArgs) => {
+  const response = await signIn({
+    email: 'meme@gmail.com',
+    password: 'Qq123123',
+  })
+
+  console.log(response)
+
   const session = await getSession(request.headers.get('Cookie'))
 
   session.set('token', 'fakeToken')
